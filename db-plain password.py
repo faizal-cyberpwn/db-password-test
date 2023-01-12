bf993b53d1b93bb24fa7ac49b14839e5f3e49d75
faizal-cyberpwn <122258089+faizal-cyberpwn@users.noreply.github.com>
2023-01-12 18:20:26 +0530

    Add files via upload

---
+++ db-plain password.py
@@ -0,0 +1,7 @@
+dbName = "<CHANGE_THIS>"
+jndiName = "jdbc/DataSource"
+dbJAASAlias = dbName + "_Alias" 
+dbUser = "<CHANGE_THIS>"
+dbPassword = "5d41402abc4b2a76b9719d911017c592"
+password=aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d
+password=9b71d224bd62f3785d96d46ad3ea3d73319bfbc2890caadae2dff72519673ca72323c3d99ba5c11d7c7acc6e14b8c5da0c4663475c2e5c3adef46f73bcdec043
